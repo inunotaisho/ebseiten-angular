1aa6bd6388c2de77723cee8dbd6313d9bce786d8
inunotaisho <ebfairweather@gmail.com>
2025-11-27 13:12:46 -0600

    Adding API and host server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_server: `https://247y8ycuyg.execute-api.us-east-2.amazonaws.com/dev`
+  api_server: `https://5g8tbwqy2i.execute-api.us-east-2.amazonaws.com/dev`,
+  host:`https://d8wbrcjhr4jz4.cloudfront.net`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api_server: `https://247y8ycuyg.execute-api.us-east-2.amazonaws.com/dev`
+  api_server: `https://5g8tbwqy2i.execute-api.us-east-2.amazonaws.com/dev`,
+  host:`https://d8wbrcjhr4jz4.cloudfront.net`
 };
 
 /*
