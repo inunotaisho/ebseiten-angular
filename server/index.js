92eb1d3b3eec365ac3d29ff81083e9560b650aac
inunotaisho <ebfairweather@gmail.com>
2026-01-16 06:29:43 -0600

    Https redirect is no longer necessary

--- server/index.js
+++ server/index.js
@@ -8,13 +8,6 @@ const app = express();
 const staticRoot = path.resolve(__dirname, '../dist/ebseiten-angular/browser');
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (!req.secure && req.headers['x-forwarded-proto'] !== 'https') {
-//     res.redirect('https://' + req.headers.host + req.url);
-//   } else {
-//     next();
-//   }
-// });
 
 // threshold controls compressing based on file size. Let's not do that
 // WARNING: The order matters here...compression must come before express.static
